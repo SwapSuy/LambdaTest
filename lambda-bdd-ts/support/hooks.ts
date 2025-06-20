faa5f096993f6a0078a40b62a35f0f899f849e96
amiteshsharma333 <amiteshsharma888@gmail.com>
2025-06-20 17:26:34 +0530

    changes done to add hooks and steps

---
+++ lambda-bdd-ts/support/hooks.ts
@@ -0,0 +1,39 @@
+import { chromium, Browser, Page } from '@playwright/test';
+import {
+  Before,
+  After,
+  AfterStep,
+  BeforeStep,
+  Status,
+  ITestCaseHookParameter
+} from '@cucumber/cucumber';
+
+interface CustomWorld {
+  page: Page;
+  browser: Browser;
+}
+
+Before(async function (this: CustomWorld) {
+  this.browser = await chromium.launch({
+    headless: false
+  });
+  const context = await this.browser.newContext();
+  this.page = await context.newPage();
+  await this.page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
+});
+
+After(async function (this: CustomWorld) {
+  await this.browser.close();
+  console.log('Browser closed in After hook');
+});
+
+AfterStep(async function (
+  this: CustomWorld,
+  { result }: ITestCaseHookParameter
+) {
+  if (result?.status === Status.FAILED) {
+    await this.page.screenshot({ path: 'screenshot1.png' });
+  }
+});
+
+BeforeStep(function () {});
